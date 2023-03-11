7b5ae0ad86cdc96a0e93bfba53a290163dcf41d3
subhradwi01 <subhradwi01@gmail.com>
2023-03-11 19:54:05 +0530

    Drone enhancement

--- src/Sections/Drone.js
+++ src/Sections/Drone.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, SpotLight } from "@react-three/drei";
+import { OrbitControls, SpotLight , Environment, Sparkles} from "@react-three/drei";
 import { DroneModel } from "../assets/DroneV3/Scene";
 const Container = styled.div`
   width: 100vw;
@@ -16,9 +16,16 @@ const Drone = () => {
   return (
     <Container>
       <Canvas>
-        <ambientLight intensity={0.8} />
+        <ambientLight intensity={1} />
+        
+      <Environment
+        // background
+        preset="warehouse"
+       />
+        <Sparkles size={2} scale={[10,10,10]} color={'#fff'}/>
         {/* <directionalLight intensity={0.4} /> */}
         <DroneModel />
+        <meshStandardMaterial color="#0a1a1f" />
         <OrbitControls />
       </Canvas>
     </Container>
